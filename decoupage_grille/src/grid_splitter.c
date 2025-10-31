47f48b32aeb791676efa6a3061fd0a4ecdd47572
AdemCelik54000 <adem.celik@epita.fr>
2025-10-31 16:17:29 +0100

    kaka

---
+++ decoupage_grille/src/grid_splitter.c
@@ -0,0 +1,429 @@
+#include <ctype.h>
+#include <dirent.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef _WIN32
+#include <direct.h>
+#define MKDIR(path) _mkdir(path)
+#else
+#include <sys/stat.h>
+#include <sys/types.h>
+#define MKDIR(path) mkdir(path, 0755)
+#endif
+
+#include "grid_splitter.h"
+
+#define STBI_ONLY_PNG
+#define STBI_ONLY_JPEG
+#define STBI_NO_GIF
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_STATIC
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-function"
+#endif
+#include "stb_image.h"
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif
+
+#define STBIW_ONLY_PNG
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_STATIC
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-function"
+#endif
+#include "stb_image_write.h"
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif
+
+typedef struct {
+    int largeur;
+    int hauteur;
+    unsigned char *pixels;
+} ImageGris;
+
+static void liberer_image_gris(ImageGris *image) {
+    if (image && image->pixels) {
+        free(image->pixels);
+        image->pixels = NULL;
+    }
+}
+
+static bool est_extension_image(const char *nom) {
+    const char *point = strrchr(nom, '.');
+    if (!point || point == nom) {
+        return false;
+    }
+    char extension[8] = {0};
+    size_t longueur = strlen(point + 1);
+    if (longueur >= sizeof(extension)) {
+        return false;
+    }
+    for (size_t i = 0; i < longueur; ++i) {
+        extension[i] = (char)tolower((unsigned char)point[1 + i]);
+    }
+    return strcmp(extension, "png") == 0 || strcmp(extension, "jpg") == 0 || strcmp(extension, "jpeg") == 0 ||
+           strcmp(extension, "bmp") == 0;
+}
+
+static int creer_repertoire(const char *chemin) {
+    if (!chemin || chemin[0] == '\0') {
+        return -1;
+    }
+    if (MKDIR(chemin) == 0) {
+        return 0;
+    }
+#ifdef _WIN32
+    if (errno == EEXIST) {
+        return 0;
+    }
+#else
+    if (errno == EEXIST) {
+        return 0;
+    }
+#endif
+    return -1;
+}
+
+static void joindre_chemin(char *destination, size_t taille_destination, const char *dossier, const char *nom) {
+    if (!dossier || dossier[0] == '\0') {
+        snprintf(destination, taille_destination, "%s", nom);
+        return;
+    }
+    size_t longueur = strlen(dossier);
+    bool besoin_sep = longueur > 0 && dossier[longueur - 1] != '/' && dossier[longueur - 1] != '\\';
+    if (besoin_sep) {
+        snprintf(destination, taille_destination, "%s/%s", dossier, nom);
+    } else {
+        snprintf(destination, taille_destination, "%s%s", dossier, nom);
+    }
+}
+
+static void nom_sans_extension(const char *fichier, char *tampon, size_t taille_tampon) {
+    const char *slash = strrchr(fichier, '/');
+    const char *backslash = strrchr(fichier, '\\');
+    const char *nom = fichier;
+    if (slash && backslash) {
+        nom = slash > backslash ? slash + 1 : backslash + 1;
+    } else if (slash) {
+        nom = slash + 1;
+    } else if (backslash) {
+        nom = backslash + 1;
+    }
+    const char *point = strrchr(nom, '.');
+    size_t longueur = point ? (size_t)(point - nom) : strlen(nom);
+    if (longueur >= taille_tampon) {
+        longueur = taille_tampon - 1;
+    }
+    memcpy(tampon, nom, longueur);
+    tampon[longueur] = '\0';
+}
+
+static unsigned char seuil_otsu(const unsigned char *pixels, size_t nombre) {
+    unsigned long histogram[256] = {0};
+    for (size_t i = 0; i < nombre; ++i) {
+        histogram[pixels[i]]++;
+    }
+
+    unsigned long total = (unsigned long)nombre;
+    unsigned long sum = 0;
+    for (int i = 0; i < 256; ++i) {
+        sum += i * histogram[i];
+    }
+
+    unsigned long sum_b = 0;
+    unsigned long w_b = 0;
+    unsigned long w_f = 0;
+    double max_var = -1.0;
+    unsigned char threshold = 0;
+
+    for (int t = 0; t < 256; ++t) {
+        w_b += histogram[t];
+        if (w_b == 0) {
+            continue;
+        }
+        w_f = total - w_b;
+        if (w_f == 0) {
+            break;
+        }
+        sum_b += t * histogram[t];
+        double m_b = (double)sum_b / (double)w_b;
+        double m_f = (double)(sum - sum_b) / (double)w_f;
+        double var_between = (double)w_b * (double)w_f * (m_b - m_f) * (m_b - m_f);
+        if (var_between > max_var) {
+            max_var = var_between;
+            threshold = (unsigned char)t;
+        }
+    }
+    return threshold;
+}
+
+static void binariser(unsigned char *pixels, size_t nombre, unsigned char seuil) {
+    for (size_t i = 0; i < nombre; ++i) {
+        pixels[i] = pixels[i] > seuil ? 255 : 0;
+    }
+}
+
+static int charger_image_gris(const char *chemin, ImageGris *resultat) {
+    if (!resultat) {
+        return -1;
+    }
+    int largeur = 0;
+    int hauteur = 0;
+    int canaux = 0;
+    unsigned char *brut = stbi_load(chemin, &largeur, &hauteur, &canaux, 0);
+    if (!brut) {
+        return -1;
+    }
+    size_t nb_pixels = (size_t)largeur * (size_t)hauteur;
+    unsigned char *gris = (unsigned char *)malloc(nb_pixels);
+    if (!gris) {
+        stbi_image_free(brut);
+        return -1;
+    }
+
+    if (canaux == 1) {
+        memcpy(gris, brut, nb_pixels);
+    } else {
+        for (size_t i = 0; i < nb_pixels; ++i) {
+            size_t indice = i * (size_t)canaux;
+            unsigned char r = brut[indice];
+            unsigned char g = canaux > 1 ? brut[indice + 1] : brut[indice];
+            unsigned char b = canaux > 2 ? brut[indice + 2] : brut[indice];
+            gris[i] = (unsigned char)(0.2126 * r + 0.7152 * g + 0.0722 * b + 0.5);
+        }
+    }
+
+    stbi_image_free(brut);
+    resultat->largeur = largeur;
+    resultat->hauteur = hauteur;
+    resultat->pixels = gris;
+    return 0;
+}
+
+static int ecrire_image_gris(const char *chemin, const unsigned char *pixels, int largeur, int hauteur) {
+    if (!pixels) {
+        return -1;
+    }
+    int stride = largeur;
+    if (stbi_write_png(chemin, largeur, hauteur, 1, pixels, stride) == 0) {
+        return -1;
+    }
+    return 0;
+}
+
+static int collecter_lignes(const unsigned char *pixels, int largeur, int hauteur, bool horizontal, int **lignes) {
+    int longueur = horizontal ? hauteur : largeur;
+    int profondeur = horizontal ? largeur : hauteur;
+    int *comptes = (int *)calloc((size_t)longueur, sizeof(int));
+    if (!comptes) {
+        return -1;
+    }
+
+    for (int i = 0; i < longueur; ++i) {
+        int compteur = 0;
+        for (int j = 0; j < profondeur; ++j) {
+            int x = horizontal ? j : i;
+            int y = horizontal ? i : j;
+            if (pixels[y * largeur + x] == 0) {
+                compteur++;
+            }
+        }
+        comptes[i] = compteur;
+    }
+
+    int valeur_max = 0;
+    for (int i = 0; i < longueur; ++i) {
+        if (comptes[i] > valeur_max) {
+            valeur_max = comptes[i];
+        }
+    }
+    if (valeur_max == 0) {
+        free(comptes);
+        return -1;
+    }
+
+    double facteur = horizontal ? 0.55 : 0.55;
+    int seuil = (int)(valeur_max * facteur);
+    int seuil_min = (int)(profondeur * 0.30);
+    if (seuil < seuil_min) {
+        seuil = seuil_min;
+    }
+
+    int *indices_lignes = (int *)malloc((size_t)longueur * sizeof(int));
+    if (!indices_lignes) {
+        free(comptes);
+        return -1;
+    }
+
+    int nb_indices = 0;
+    int depart_bloc = -1;
+    for (int i = 0; i < longueur; ++i) {
+        if (comptes[i] >= seuil) {
+            if (depart_bloc < 0) {
+                depart_bloc = i;
+            }
+        } else if (depart_bloc >= 0) {
+            int fin_bloc = i - 1;
+            indices_lignes[nb_indices++] = (depart_bloc + fin_bloc) / 2;
+            depart_bloc = -1;
+        }
+    }
+    if (depart_bloc >= 0) {
+        int fin_bloc = longueur - 1;
+        indices_lignes[nb_indices++] = (depart_bloc + fin_bloc) / 2;
+    }
+
+    free(comptes);
+    if (nb_indices < 2) {
+        free(indices_lignes);
+        return -1;
+    }
+
+    *lignes = indices_lignes;
+    return nb_indices;
+}
+
+static int decouper_grille(const char *chemin_entree, const char *repertoire_sortie) {
+    ImageGris image = {0};
+    if (charger_image_gris(chemin_entree, &image) != 0) {
+        return -1;
+    }
+    size_t nombre_pixels = (size_t)image.largeur * (size_t)image.hauteur;
+    unsigned char seuil = seuil_otsu(image.pixels, nombre_pixels);
+    binariser(image.pixels, nombre_pixels, seuil);
+
+    int *lignes_horizontales = NULL;
+    int *lignes_verticales = NULL;
+    int nb_lignes_h = collecter_lignes(image.pixels, image.largeur, image.hauteur, true, &lignes_horizontales);
+    int nb_lignes_v = collecter_lignes(image.pixels, image.largeur, image.hauteur, false, &lignes_verticales);
+
+    char nom_base[256] = {0};
+    nom_sans_extension(chemin_entree, nom_base, sizeof(nom_base));
+
+    char repertoire_grille[512] = {0};
+    joindre_chemin(repertoire_grille, sizeof(repertoire_grille), repertoire_sortie, nom_base);
+
+    const int marge = 1;
+    if (nb_lignes_h < 2 || nb_lignes_v < 2) {
+        free(lignes_horizontales);
+        free(lignes_verticales);
+        liberer_image_gris(&image);
+        return -1;
+    }
+
+    if (creer_repertoire(repertoire_grille) != 0) {
+        free(lignes_horizontales);
+        free(lignes_verticales);
+        liberer_image_gris(&image);
+        return -1;
+    }
+
+    int cases_enregistrees = 0;
+    for (int ligne = 0; ligne < nb_lignes_h - 1; ++ligne) {
+        int marge_haut = ligne > 0 ? marge : 0;
+        int marge_bas = (ligne + 1 < nb_lignes_h - 1) ? marge : 0;
+        int bord_haut = lignes_horizontales[ligne] + marge_haut;
+        int bord_bas = lignes_horizontales[ligne + 1] - marge_bas;
+        if (bord_bas <= bord_haut) {
+            continue;
+        }
+        for (int colonne = 0; colonne < nb_lignes_v - 1; ++colonne) {
+            int marge_gauche = colonne > 0 ? marge : 0;
+            int marge_droite = (colonne + 1 < nb_lignes_v - 1) ? marge : 0;
+            int bord_gauche = lignes_verticales[colonne] + marge_gauche;
+            int bord_droit = lignes_verticales[colonne + 1] - marge_droite;
+            if (bord_droit <= bord_gauche) {
+                continue;
+            }
+            int largeur_case = bord_droit - bord_gauche + 1;
+            int hauteur_case = bord_bas - bord_haut + 1;
+            unsigned char *case_pixels = (unsigned char *)malloc((size_t)largeur_case * (size_t)hauteur_case);
+            if (!case_pixels) {
+                free(lignes_horizontales);
+                free(lignes_verticales);
+                liberer_image_gris(&image);
+                return -1;
+            }
+
+            for (int y = 0; y < hauteur_case; ++y) {
+                memcpy(case_pixels + (size_t)y * (size_t)largeur_case,
+                       image.pixels + (size_t)(bord_haut + y) * (size_t)image.largeur + (size_t)bord_gauche,
+                       (size_t)largeur_case);
+            }
+
+            char nom_fichier_case[512] = {0};
+            snprintf(nom_fichier_case, sizeof(nom_fichier_case), "x%d_y%d.png", colonne, ligne);
+            char chemin_complet[1024] = {0};
+            joindre_chemin(chemin_complet, sizeof(chemin_complet), repertoire_grille, nom_fichier_case);
+            if (ecrire_image_gris(chemin_complet, case_pixels, largeur_case, hauteur_case) == 0) {
+                cases_enregistrees++;
+            }
+            free(case_pixels);
+        }
+    }
+
+    free(lignes_horizontales);
+    free(lignes_verticales);
+    liberer_image_gris(&image);
+    return cases_enregistrees > 0 ? 0 : -1;
+}
+
+static int pour_chaque_image(const char *repertoire_entree,
+                          int (*fonction_rappel)(const char *chemin_entree, const char *nom_fichier, void *ctx), void *ctx) {
+    DIR *dir = opendir(repertoire_entree);
+    if (!dir) {
+        return -1;
+    }
+    struct dirent *entree = NULL;
+    int statut = 0;
+    while ((entree = readdir(dir)) != NULL) {
+        if (entree->d_name[0] == '.') {
+            continue;
+        }
+        if (!est_extension_image(entree->d_name)) {
+            continue;
+        }
+        char chemin_entree[1024] = {0};
+        joindre_chemin(chemin_entree, sizeof(chemin_entree), repertoire_entree, entree->d_name);
+        if (fonction_rappel(chemin_entree, entree->d_name, ctx) != 0) {
+            statut = -1;
+        }
+    }
+    closedir(dir);
+    return statut;
+}
+
+typedef struct {
+    const char *repertoire_sortie;
+} ContexteDecoupe;
+
+static int rappel_decoupe(const char *chemin_entree, const char *nom_fichier_case, void *ctx) {
+    (void)nom_fichier_case;
+    ContexteDecoupe *contexte = (ContexteDecoupe *)ctx;
+    return decouper_grille(chemin_entree, contexte->repertoire_sortie);
+}
+
+int decouper_lettres_dans_repertoire(const char *repertoire_entree, const char *repertoire_sortie) {
+    if (creer_repertoire(repertoire_sortie) != 0) {
+        return -1;
+    }
+    ContexteDecoupe ctx = {.repertoire_sortie = repertoire_sortie};
+    return pour_chaque_image(repertoire_entree, rappel_decoupe, &ctx);
+}
+
+int main(int argc, char **argv) {
+    const char *entree_par_defaut = "data/clean_grid";
+    const char *sortie_par_defaut = "data/lettres";
+    const char *repertoire_entree = argc >= 2 ? argv[1] : entree_par_defaut;
+    const char *repertoire_sortie = argc >= 3 ? argv[2] : sortie_par_defaut;
+    return decouper_lettres_dans_repertoire(repertoire_entree, repertoire_sortie) == 0 ? 0 : 1;
+}
--- decoupage_grille/src/grid_splitter.h
+++ decoupage_grille/src/grid_splitter.h
@@ -1,38 +1,14 @@
+/* grid_splitter.h
+ *
+ * Provides the public interface for the grid cropping and letter splitting
+ * utilities used in the decoupage_grille project.
+ */
+
 #ifndef GRID_SPLITTER_H
 #define GRID_SPLITTER_H
 
-#include <stddef.h>
+int crop_grids_in_directory(const char *input_dir, const char *output_dir);
+int split_letters_in_directory(const char *input_dir, const char *output_dir);
 
-typedef struct {
-    int deb;
-    int fin;
-} Bande;
+#endif /* GRID_SPLITTER_H */
 
-int creer_dossier(const char *chem);
-int vider_dossier(const char *chem);
-int pret_dossier(const char *chem);
-
-unsigned char *charger_image(const char *chem, int *larg, int *haut, int *canaux);
-void liberer_image(unsigned char *img);
-int ecrire_png(const char *chem, int larg, int haut, int comp, const void *donnees, int stride);
-
-Bande *trouve_band_h(const unsigned char *pix, int larg, int haut, size_t *nb);
-Bande *trouve_band_v(const unsigned char *pix, int larg, int haut, size_t *nb);
-void liberer_bandes(Bande *bandes);
-
-unsigned char *prendre_case(const unsigned char *img,int larg_img,int x0,int y0, int x1,int y1,int *ret_larg, int *ret_haut);
-int sauver_case(const char *dossier, size_t col, size_t lig, const unsigned char *pix,  int larg,int haut,int stride);
-int couper_cases(const unsigned char *img, int larg,int haut,const Bande *band_h, size_t nb_h,const Bande *band_v, 
-    size_t nb_v,int marge, const char *dossier,
-    double *tab_larg,double *tab_haut,size_t max_cases,size_t *nb_sortie);
-int uniformiser_cases(const char *dossier);
-
-int cases_uniformes(const double *tab_larg,const double *tab_haut,
-                    size_t nb);
-
-int decoupe_lettres(const unsigned char *img,int larg,int haut,
-                    const char *dossier);
-int decoupe_grille(const unsigned char *img,
-                   int larg,int haut,const char *dossier);
-
-#endif
